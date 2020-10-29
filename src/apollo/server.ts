f47c8b96f813f516e50dc9caf99598f1d041a7c6
Samantha O'Gorman <samogorm@icloud.com>
2020-10-29 19:06:33 +0000

    Add role schema to graphql server.

--- src/apollo/server.ts
+++ src/apollo/server.ts
@@ -2,11 +2,11 @@ import { ApolloServer } from 'apollo-server-express';
 const { applyMiddleware } = require('graphql-middleware');
 
 import { Mutation, Query } from './types';
-import { UserSchema, ClientSchema, TokenSchema } from './schemas';
+import { UserSchema, ClientSchema, TokenSchema, RoleSchema } from './schemas';
 import { UserResolver, ClientResolver, RoleResolver } from './resolvers';
 
 const apolloServer = () => {
-  const typeDefs = [ Mutation, Query, UserSchema, ClientSchema, TokenSchema ];
+  const typeDefs = [ Mutation, Query, UserSchema, ClientSchema, TokenSchema, RoleSchema ];
   const resolvers = {
     Query: { ...UserResolver.Query, ...ClientResolver.Query, ...RoleResolver.Query },
     Mutation: { ...UserResolver.Mutation, ...ClientResolver.Mutation, ...RoleResolver.Mutation }
